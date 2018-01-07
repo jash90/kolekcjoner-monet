5d82ff69e383ddabb18175a6541bce4d7df1dfec
Bartłomiej Zimny <bartekziimny90@gmail.com>
2018-01-07 03:12:20 +0100

    Dodanie firebase dodanie rejestracji przez firebase dodanie modalloading

---
+++ src/components/ModalLoading.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {View, Modal, ActivityIndicator, Text, StyleSheet} from 'react-native';
+class ModalLoading extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Modal visible={this.props.visible} transparent={true}>
+                <View style={styles.overlay}>
+                    <View style={styles.container}>
+                        <View>
+                            <ActivityIndicator/>
+                        </View>
+                        <View style={styles.text}>
+                            <Text>
+                                {this.props.text}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+        );
+    }
+}
+
+export default ModalLoading;
+
+const styles = StyleSheet.create({
+    text: {
+        marginLeft: 10
+    },
+    container: {
+        padding: 20,
+        backgroundColor: "#fff",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    overlay: {
+        width: '100%',
+        height: '100%',
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
--- src/scene/RegisterScene.js
+++ src/scene/RegisterScene.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, View, ToastAndroid} from 'react-native'
+import {
+    Platform,
+    StyleSheet,
+    View,
+    ToastAndroid,
+    Modal,
+    ActivityIndicator
+} from 'react-native'
 import {
     Container,
     Header,
@@ -15,15 +22,19 @@ import {
     Text,
     Drawer
 } from 'native-base';
+import firebase from 'react-native-firebase';
 import {Actions} from 'react-native-router-flux';
+import ModalLoading from '../components/ModalLoading';
 class LoginScene extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            login: '',
-            email: '',
-            password: '',
-            repeatpassword: ''
+            login: 'rafal',
+            email: 'rafal@gg.pl',
+            password: '123456',
+            repeatpassword: '123456',
+            error: '',
+            loading: false
         }
     }
     render() {
@@ -50,21 +61,27 @@ class LoginScene extends Component {
                     <Form>
                         <Item floatingLabel>
                             <Label>Login</Label>
-                            <Input onChangeText={(text) => this.setState({login: text})}/>
+                            <Input
+                                value={this.state.login}
+                                onChangeText={(text) => this.setState({login: text})}/>
                         </Item>
                         <Item floatingLabel>
                             <Label>Email</Label>
-                            <Input onChangeText={(text) => this.setState({email: text})}/>
+                            <Input
+                                value={this.state.email}
+                                onChangeText={(text) => this.setState({email: text})}/>
                         </Item>
                         <Item floatingLabel last>
                             <Label>Hasło</Label>
                             <Input
+                                value={this.state.password}
                                 secureTextEntry={true}
                                 onChangeText={(text) => this.setState({password: text})}/>
                         </Item>
                         <Item floatingLabel last>
                             <Label>Powtórz hasło</Label>
                             <Input
+                                value={this.state.repeatpassword}
                                 secureTextEntry={true}
                                 onChangeText={(text) => this.setState({repeatpassword: text})}/>
                         </Item>
@@ -79,6 +96,10 @@ class LoginScene extends Component {
                             Zarejestruj
                         </Text>
                     </Button>
+                    <ModalLoading
+                        visible={this.state.loading}
+                        text={"Rejestracja..."}
+                    />
                 </View>
             </Container>
         );
@@ -108,8 +129,20 @@ class LoginScene extends Component {
             alert('Email jest niepoprawny.');
             return;
         }
-        ToastAndroid.show('Konto ' + this.state.login + ' zostało utworzone.', ToastAndroid.SHORT);
-        Actions.LoginScene();
+        this.setState({loading: true});
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(() => {
+                ToastAndroid.show('Konto ' + this.state.login + ' zostało utworzone.', ToastAndroid.SHORT);
+                Actions.LoginScene();
+                this.setState({error: '', loading: false});
+            })
+            .catch((error) => {
+                this.setState({error: 'Authentication failed.', loading: false});
+                ToastAndroid.show(error.message, ToastAndroid.SHORT);
+            });
+
     }
 }
 
