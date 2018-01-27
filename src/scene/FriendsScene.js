21d5b00c5f8b9745e071ed78d678535d631559ab
Bartłomiej Zimny <b.zimny@ideo.pl>
2018-01-27 18:48:52 +0100

    dodanie czytania użytkowników z bazy

--- src/scene/FriendsScene.js
+++ src/scene/FriendsScene.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, View, Image} from 'react-native'
+import React, { Component } from "react";
+import { Platform, StyleSheet, View, Image } from "react-native";
 import {
   Container,
   Header,
@@ -18,80 +18,131 @@ import {
   ListItem,
   Thumbnail,
   List
-} from 'native-base';
-import SideBar from '../components/SideBar';
-import user from '../img/logo.png';
-import collection from '../img/drawer-cover.png';
-var items = ['Bartek', 'Justyna', 'Rafał', 'Marek', 'Wojtek'];
+} from "native-base";
+import SideBar from "../components/SideBar";
+import user from "../img/logo.png";
+import collection from "../img/drawer-cover.png";
+import firebase from "react-native-firebase";
+var items = ["Bartek", "Justyna", "Rafał", "Marek", "Wojtek"];
 class FriendsScene extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      followers: []
+    };
+    this.ref = firebase.firestore().collection("followers");
+    this.storage = firebase.storage();
+    this.unsubscribe = null;
+  }
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  onCollectionUpdate = querySnapshot => {
+    const followers = [];
+    firebase
+      .firestore()
+      .doc("followers/NbcZVRCcpRWAjOsx47L7CJArCH83")
+      .get()
+      .then(doc => {
+        const { users } = doc.data();
+        users.forEach(user => {
+         user.get().then(e => {
+            followers.push(e.data());
+                 this.setState({ followers });
+          });
+        });
+      });
+     
+  };
+
   render() {
     return (
       <Container>
-        <Header >
+        <Header>
           <Left>
-            <Button transparent onPress={() => this.openDrawer()}><Icon name={'ios-menu'} style={{
-        color: '#fff'
-      }}/></Button>
+            <Button transparent onPress={() => this.openDrawer()}>
+              <Icon
+                name={"ios-menu"}
+                style={{
+                  color: "#fff"
+                }}
+              />
+            </Button>
           </Left>
           <Body>
-            <Text style={{
-              color: '#fff'
-            }}>Znajomi</Text>
+            <Text
+              style={{
+                color: "#fff"
+              }}
+            >
+              Znajomi
+            </Text>
           </Body>
         </Header>
         <Drawer
-          ref={(ref) => {
-          this.drawer = ref;
-        }}
-          content={< SideBar />}
-          onClose={() => this.closeDrawer()}>
-          <Content style={{
-            backgroundColor: '#fff'
-          }}>
+          ref={ref => {
+            this.drawer = ref;
+          }}
+          content={<SideBar />}
+          onClose={() => this.closeDrawer()}
+        >
+          <Content
+            style={{
+              backgroundColor: "#fff"
+            }}
+          >
             <List
-              dataArray={items}
-              renderRow={(item) => <View
-              style={{
-              justifyContent: 'center',
-              flexDirection: 'row',
-              alignSelf: 'flex-start'
-            }}>
-              <View style={{
-                justifyContent: 'center'
-              }}>
-                <Icon
-                  name={'ios-contact'}
+              dataArray={this.state.followers}
+              renderRow={(item) => (
+                <View
                   style={{
-                  fontSize: 60,
-                  marginLeft: 10,
-                  marginRight: 10
-                }}/>
-              </View>
-              <View
-                style={{
-                alignItems: 'flex-start',
-                justifyContent: 'center'
-              }}>
-                <Text>{item}</Text>
-                <Text note>Kolekcja Starych Monet, Monety z II Wojny Światowej</Text>
-              </View>
-            </View>}/>
+                    justifyContent: "center",
+                    flexDirection: "row",
+                    alignSelf: "flex-start"
+                  }}
+                >
+                  <View
+                    style={{
+                      justifyContent: "center"
+                    }}
+                  >
+                    <Icon
+                      name={"ios-contact"}
+                      style={{
+                        fontSize: 60,
+                        marginLeft: 10,
+                        marginRight: 10
+                      }}
+                    />
+                  </View>
+                  <View
+                    style={{
+                      alignItems: "flex-start",
+                      justifyContent: "center"
+                    }}
+                  >
+                    <Text>{item.firstname +" "+ item.lastname}</Text>
+                    <Text note>
+                     {"Miasto: "+item.city+" Email: "+item.email}
+                    </Text>
+                  </View>
+                </View>
+              )}
+            />
           </Content>
         </Drawer>
       </Container>
     );
   }
   closeDrawer = () => {
-    this
-      .drawer
-      ._root
-      .close();
+    this.drawer._root.close();
   };
   openDrawer = () => {
-    this
-      .drawer
-      ._root
-      .open();
+    this.drawer._root.open();
   };
 }
 
